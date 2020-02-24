16999690753af9a666f68dc105d80afc9e4c1723
Isaac Morrell <c3355530@uon.edu.au>
2020-02-24 19:41:35 +1100

    Initial Commit

---
+++ TestProject/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    int main()
+{
+    float x;
+    float y;
+    printf("Enter an number: ");
+    scanf("%f", &x);
+    y=2*x+3*5;
+    printf("y 1= %f\n", y);
+    return 0;
+}
